b0ff4052672eeae69dd63dfa9e931a02ea27e745
Davina Leong <davina.leong.sy@gmail.com>
2022-08-10 15:13:03 +0800

    Done with random choice selector

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,11 @@
+const settings = {
+  separator: `,`,
+  speed: 100,
+  times: 30,
+};
+
 const formFieldElement = document.querySelector("textarea[name='values']");
 const tagsElement = document.querySelector("[data-tags]");
-const separator = `,`;
 
 formFieldElement.focus();
 
@@ -17,19 +22,49 @@ const createTags = (input) => {
   console.log(`== Create Tags ==`);
 
   const tags = input
-    .split(separator)
+    .split(settings.separator)
     .filter((tag) => tag.trim() !== ``)
     .map((tag) => tag.trim());
 
   tagsElement.innerText = ``;
-  tags.forEach((tag) => {
+  tags.forEach((tag, index) => {
     const tagElement = document.createElement(`div`);
     tagElement.classList.add(`tag`);
+    tagElement.setAttribute(`data-tag`, index);
     tagElement.innerText = tag;
     tagsElement.appendChild(tagElement);
   });
 };
 
 const randomSelect = () => {
-  console.log(`== Random Select ==`);
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+    highlightTag(randomTag);
+
+    setTimeout(() => {
+      unhighlightTag(randomTag);
+    }, settings.speed);
+  }, settings.speed);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+      highlightTag(randomTag);
+    }, 100);
+  }, settings.times * settings.speed);
+};
+
+const pickRandomTag = () => {
+  const tags = document.querySelectorAll(`[data-tag]`);
+  return tags[Math.floor(Math.random() * tags.length)];
+};
+
+const highlightTag = (tag) => {
+  tag.setAttribute(`data-selected`, true);
+};
+
+const unhighlightTag = (tag) => {
+  tag.removeAttribute(`data-selected`);
 };
