89e09d2a8b987e9df6b5a22ce98a0d1be4ad36ce
Davina Leong <davina.leong.sy@gmail.com>
2022-08-10 14:41:55 +0800

    Able to generate tags from input

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,22 @@
-const formField = document.querySelector("textarea[name='values']");
-const tags = document.querySelector("[data-tags]");
+const formFieldElement = document.querySelector("textarea[name='values']");
+const tagsElement = document.querySelector("[data-tags]");
+const separator = `,`;
+
+formFieldElement.focus();
+
+formFieldElement.addEventListener(`input`, (e) => createTags(e.target.value));
+
+const createTags = (input) => {
+  const tags = input
+    .split(separator)
+    .filter((tag) => tag.trim() !== ``)
+    .map((tag) => tag.trim());
+
+  tagsElement.innerText = ``;
+  tags.forEach((tag) => {
+    const tagElement = document.createElement(`div`);
+    tagElement.classList.add(`tag`);
+    tagElement.innerText = tag;
+    tagsElement.appendChild(tagElement);
+  });
+};
